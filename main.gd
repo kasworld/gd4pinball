d8e70f5258a1f5b057c535612bf545cb5007d63e
Kang Seukwon <kasworld@gmail.com>
2025-07-08 08:38:56 +0900

    월드 범위 변경

--- main.gd
+++ main.gd
@@ -24,7 +24,7 @@ var ball_end_count :Array = []
 func _ready() -> void:
 	dark_colors = NamedColorList.make_dark_color_list(0.5)
 	$Arrow3D.init(2,Color.RED,0.1,0.3)
-	$Arrow3D.position = Vector3(0.25, -8, -9.5)
+	$Arrow3D.position = Vector3(10, 2, 0.5)
 	add_bars()
 	
 func add_bars() -> void:
@@ -33,9 +33,9 @@ func add_bars() -> void:
 		for y in range(-8,11):
 			var b = preload("res://bar.tscn").instantiate().set_color(dark_colors.pick_random()[0])
 			if y % 2 == 0:
-				b.position = Vector3(x-0.25, -9.5, y-0.25)
+				b.position = Vector3(x-0.25, -9.5, y-0.25) + Vector3(10,10,10)
 			else :
-				b.position = Vector3(x+0.25, -9.5, y-0.25)
+				b.position = Vector3(x+0.25, -9.5, y-0.25) + Vector3(10,10,10)
 			b.set_default_pos(b.position) 
 			$BarContainer.add_child(b)
 		var lb = Label3D.new()
@@ -43,7 +43,7 @@ func add_bars() -> void:
 		lb.pixel_size = 0.01
 		lb.billboard = BaseMaterial3D.BILLBOARD_ENABLED
 		lb.no_depth_test = true
-		lb.position = Vector3(x+0.25, -9.5, 10)
+		lb.position = Vector3(x+0.25, -9.5, 10) + Vector3(10,10,10)
 		$BallEndCounterContainer.add_child(lb)
 		ball_end_count.append(0)
 
@@ -60,7 +60,7 @@ func add_ball() -> void:
 	d.ball_ended.connect(ball_ended)
 
 func ball_ended(pos :Vector3) -> void:
-	var i = int(pos.x+10) 
+	var i = int(pos.x) 
 	ball_end_count[i] += 1
 	$BallEndCounterContainer.get_child(i).text = "%s" % ball_end_count[i]
 
@@ -69,7 +69,7 @@ func _process(delta: float) -> void:
 	update_label()
 	var t = Time.get_unix_time_from_system() /-3.0
 	if camera_move:
-		$Camera3D.position = Vector3(sin(t)*10, sin(t)*5-2, cos(t)*10)
+		$Camera3D.position = Vector3(sin(t)*10, sin(t)*5-2, cos(t)*10) + Vector3(10,10,10)
 		$Camera3D.look_at(Vector3(0,-10,0))
 
 func update_label() -> void:
@@ -116,8 +116,8 @@ func _on_카메라변경_pressed() -> void:
 		reset_camera_pos()
 
 func reset_camera_pos()->void:
-	$Camera3D.position = Vector3(0,3,7)
-	$Camera3D.look_at(Vector3(0,-10,2))
+	$Camera3D.position = Vector3(10,13,17)
+	$Camera3D.look_at(Vector3(10,0,12))
 	$Camera3D.far = 50
 
 func _on_timer공추가_timeout() -> void:
